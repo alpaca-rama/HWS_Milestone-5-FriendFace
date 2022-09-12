c96d08cd360c892a105f5ce58ebde1aa7817c91b
Luca <luca@weve-got-it.com>
2022-09-12 21:41:08 +0200

    Initial Commit

---
+++ Milestone-5-FriendFace/Milestone_5_FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone_5_FriendFaceApp.swift
+//  Milestone-5-FriendFace
+//
+//  Created by Luca Capriati on 2022/09/12.
+//
+
+import SwiftUI
+
+@main
+struct Milestone_5_FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
